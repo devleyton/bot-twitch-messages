ab5a78a20f0205cace34a2b08b69479ea89118f0
devleyton <klaysitoprivado@gmail.com>
2022-05-03 17:24:28 -0500

    la subida inicial

---
+++ botsito.py
@@ -0,0 +1,97 @@
+
+#Importar librerias
+import pyautogui
+import time
+import random
+
+#Esperar 5 segundos pa iniciar
+time.sleep (5)
+
+pyautogui.typewrite("INICIANDO EL BOTSITO XD")
+pyautogui.press("enter")
+time.sleep (10)
+
+#Bucle para 3 horas
+n = 1
+while n < 10000:
+    r = random.randint(1,15)
+    if r == 1:
+        pyautogui.typewrite("Esta super cool eso")
+        pyautogui.press("enter")
+        time.sleep (30)
+
+    if r == 2:
+        pyautogui.typewrite("Bueeee amigo, sos re pro")
+        pyautogui.press("enter")
+        time.sleep (30)
+
+    if r == 3:
+        pyautogui.typewrite("Ijole!")
+        pyautogui.press("enter")
+        time.sleep (30)
+
+    if r == 4:
+        pyautogui.typewrite("K pro")
+        pyautogui.press("enter")
+        time.sleep (30)
+
+    if r == 5:
+        pyautogui.typewrite("JAJAJAJA")
+        pyautogui.press("enter")
+        time.sleep (30)
+
+    if r == 6:
+        pyautogui.typewrite("AAHHHHHHH!!!")
+        pyautogui.press("enter")
+        time.sleep (30)
+
+    if r == 7:
+        pyautogui.typewrite("Aqui estoy activo mi pai")
+        pyautogui.press("enter")
+        time.sleep (30)
+
+    if r == 8:
+        pyautogui.typewrite("Van a jugar o k")
+        pyautogui.press("enter")
+        time.sleep (30)
+
+    if r == 9:
+        pyautogui.typewrite("Eso tilin!")
+        pyautogui.press("enter")
+        time.sleep (30)
+
+    if r == 10:
+        pyautogui.typewrite("Nah amigo que fue eso?")
+        pyautogui.press("enter")
+        time.sleep (30)
+
+    if r == 11:
+        pyautogui.typewrite("XD")
+        pyautogui.press("enter")
+        time.sleep (30)
+
+    if r == 12:
+        pyautogui.typewrite("!discord")
+        pyautogui.press("enter")
+        time.sleep (30)
+    
+    if r == 13:
+        pyautogui.typewrite("!redes")
+        pyautogui.press("enter")
+        time.sleep (30)
+
+    if r == 14:
+        pyautogui.typewrite("Nasheeee")
+        pyautogui.press("enter")
+        time.sleep (30)
+
+    if r == 15:
+        pyautogui.typewrite("XD")
+        pyautogui.press("enter")
+        time.sleep (30)
+
+    time.sleep (10)
+    n = n + 1
+
+
+
